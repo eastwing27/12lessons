2753a9ae50bf7ab6b5d8ee6c3307ea8d43f3ac81
Mikhail Chernienko <eastwing27@gmail.com>
2017-11-02 11:48:40 +1000

    template fixed

--- 03_SpotifyApp/app/app.module.ts
+++ 03_SpotifyApp/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/pages/home.component'
-import { AboutComponent } from './components/pages/about.component'
-import { NavbarComponent } from './components/navbar/navbar.component'
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component'
+
 
 @NgModule({
     imports: [BrowserModule, routing ],
     declarations: [
-        AppComponent, 
-        HomeComponent,
-        AboutComponent,
-        NavbarComponent, 
-        JumbotronComponent ],
+        AppComponent
+     ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
